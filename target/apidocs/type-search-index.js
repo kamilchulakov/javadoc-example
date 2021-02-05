dd89273e940c5406dba5ecab780dce8830a1d52a
ulyanovskk <mr1english4@gmail.com>
2021-02-05 21:35:06 +0400

    Built: javadoc

---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"objects","l":"AbleToDoBadThings"},{"p":"objects","l":"AbleToDoGoodThings"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"main","l":"Main"},{"p":"objects","l":"Person"}];updateSearchResults();
